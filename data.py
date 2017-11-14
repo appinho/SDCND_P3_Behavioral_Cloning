cf1132079d36883cf03da328c36619100a946569
Simon Appel <simon.appel@daimler.com>
2017-11-14 15:08:45 -0800

    Added NVIDIA net

--- data.py
+++ data.py
@@ -2,12 +2,27 @@
 import csv
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 # define path
-data_path = '../Simulated Data/'
-
+data_path = '../Simulated Data Old/'
+data_augmentation = True
+use_multiple_cameras = True
+correction_factor = 0.2
 
 def load_data():
+    images,measurements = collect_data()
+
+    # Define training data
+    X_train = np.array(images)
+    y_train = np.array(measurements)
+
+    print('Shape image data: ', X_train.shape)
+    print('Shape measurement data: ', y_train.shape)
+
+    return X_train,y_train
+
+def collect_data():
     # init csv file buffer
     lines = []
 
@@ -16,7 +31,7 @@ def load_data():
         reader = csv.reader(csvfile)
         for line in reader:
             lines.append(line)
-
+            
     # init image and measurement buffer
     images = []
     measurements = []
@@ -30,18 +45,49 @@ def load_data():
         images.append(image)
         measurement = float(line[3])
         measurements.append(measurement)
+        if use_multiple_cameras:
+            if not line[1] == "":
+                source_path = line[1]
+                file_name = source_path.split('/')[-1]
+                current_path = data_path + 'IMG/' + file_name
+                image = cv2.imread(current_path)
+                images.append(image)
+                measurement = float(line[3]) + correction_factor
+                measurements.append(measurement)
+            if not line[2] == "":
+                source_path = line[2]
+                file_name = source_path.split('/')[-1]
+                current_path = data_path + 'IMG/' + file_name
+                image = cv2.imread(current_path)
+                images.append(image)
+                measurement = float(line[3]) - correction_factor
+                measurements.append(measurement)
 
-    # Define training data
-    X_train = np.array(images)
-    y_train = np.array(measurements)
+                
 
-    print('Shape image data: ', X_train.shape)
-    print('Shape measurement data: ', y_train.shape)
+    # data augmentation
+    if data_augmentation:
+        extend_with_flipped_images(images,measurements)
+        
+    return images,measurements
 
-    return X_train,y_train
 
-def mirror_data():
-    augmentated_images = []
-    augmentated_labels = []
+def extend_with_flipped_images(images,measurements):
 
-    
+    print("Before data augmentation = " + str(len(images)))
+    # determine
+    number_of_images = len(images)
+    for i in range(number_of_images):
+        # flipped data
+        images.append(cv2.flip(images[i],1))
+        measurements.append(measurements[i]*-1.0)
+        
+    print("After data augmentation = " + str(len(images)))
+
+def draw_data(X,y):
+    n, bins, patches = plt.hist(y, 29, facecolor='g', alpha=0.75)
+    plt.title('Histogram of steering angles')
+    plt.grid(True)
+    plt.show()
+
+
--- net.py
+++ net.py
@@ -1,8 +1,9 @@
 from keras.models import Sequential
-from keras.layers import Flatten,Dense,Lambda,Convolution2D,MaxPooling2D
+from keras.layers import Flatten,Dense,Lambda,Convolution2D,MaxPooling2D, Cropping2D
 
 # Hyperparameters
 ratio_validation_set = 0.3
+batch_size = 128
 
 def one_layer_net(X_train,y_train,num_epochs,model_name):
     # define network
@@ -31,7 +32,8 @@ def le_net(X_train,y_train,num_epochs,model_name):
     input_shape = X_train.shape[1:4]
     print(input_shape)
     model = Sequential()
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=input_shape))
+    model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=input_shape))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5))
     model.add(Convolution2D(6,5,5,activation='relu'))
     print(model.output_shape)
     model.add(MaxPooling2D())
@@ -42,8 +44,55 @@ def le_net(X_train,y_train,num_epochs,model_name):
     print(model.output_shape)
     model.add(Flatten())
     print(model.output_shape)
-    model.add(Dense(120))
-    model.add(Dense(84))
+    model.add(Dense(1200,activation='relu'))
+    model.add(Dense(84,activation='relu'))
+    model.add(Dense(1))
+
+    train_model(model,X_train,y_train,num_epochs,model_name)
+
+def nvidia_net(X_train,y_train,num_epochs,model_name):
+    input_shape = X_train.shape[1:4]
+    print(input_shape)
+    model = Sequential()
+    # Cropping images
+    model.add(Cropping2D(cropping=((64, 30), (60, 60)), input_shape=input_shape))
+    print(model.output_shape)
+
+    # Normalization
+    model.add(Lambda(lambda x: x / 255.0 - 0.5))
+
+    # 1st 5x5 Convolution
+    model.add(Convolution2D(24,5,5,activation='relu'))
+    model.add(MaxPooling2D(border_mode='same'))
+    print(model.output_shape)
+
+    # 2nd 5x5 Convolution
+    model.add(Convolution2D(36,5,5,activation='relu'))
+    model.add(MaxPooling2D(border_mode='same'))
+    print(model.output_shape)
+
+    # 3rd 5x5 Convolution
+    model.add(Convolution2D(48,5,5,activation='relu'))
+    model.add(MaxPooling2D(border_mode='same'))
+    print(model.output_shape)
+
+    # 4th 3x3 Convolution
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    print(model.output_shape)
+
+    # 5th 3x3 Convolution
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    print(model.output_shape)
+
+    # Flatten
+    model.add(Flatten())
+    print(model.output_shape)
+
+    # Fully connected layers
+    model.add(Dense(1164))
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
     model.add(Dense(1))
 
     train_model(model,X_train,y_train,num_epochs,model_name)
@@ -52,7 +101,7 @@ def le_net(X_train,y_train,num_epochs,model_name):
 def train_model(model,X_train,y_train,num_epochs,model_name):
     # define loss and optimizer and train the model
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train,y_train,validation_split=ratio_validation_set,shuffle=True,nb_epoch=num_epochs)
+    model.fit(X_train,y_train,batch_size=batch_size,validation_split=ratio_validation_set,shuffle=True,nb_epoch=num_epochs)
 
     # save model
     model.save(model_name)
