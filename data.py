02c868d7ecd4664b5edd075f75790bf3ccaacaed
Simon Appel <simon.appel@daimler.com>
2017-11-20 16:20:22 -0800

    Bug fix of BGR to RGB reading with matplotlib

--- data.py
+++ data.py
@@ -3,9 +3,15 @@ import csv
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 # define path
-directory = '../Simulated Data/'
+directory = '../Recorded_Data/'
+data_paths = ['Loop_Data/','Loop_Data_Reversed/',
+              'Recover_Data/','Recover_Data_Reversed/',
+              'Bridge_Recover_Data/','Curve_Recover_Data/',
+              'Recover_Data_2/', 'Loop_Data_2/']
+data_usage = [True,True,True,True,True,True,True,True]
 data_augmentation = True
 use_multiple_cameras = True
 correction_factor = 0.2
@@ -28,40 +34,44 @@ def collect_data():
     measurements = []
 
     # open csv file from simulation data
-    # init csv file buffer
-    lines = []
+    for counter,data_path in enumerate(data_paths):
+        if not data_usage[counter]:
+            continue
+        print(directory + data_path + "loading..")
+        # init csv file buffer
+        lines = []
 
-    with open(directory + 'driving_log.csv') as csvfile:
-        reader = csv.reader(csvfile)
-        for line in reader:
-            lines.append(line)
+        with open(directory + data_path + 'driving_log.csv') as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                lines.append(line)
 
-    # loop through frames and store images and measurements
-    for line in lines[:1000]:
-        source_path = line[0]
-        file_name = source_path.split('/')[-1]
-        current_path = directory + 'IMG/' + file_name
-        image = cv2.imread(current_path)
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
-        if use_multiple_cameras:
-            if not line[1] == "":
-                source_path = line[1]
-                file_name = source_path.split('/')[-1]
-                current_path = directory + 'IMG/' + file_name
-                image = cv2.imread(current_path)
-                images.append(image)
-                measurement = float(line[3]) + correction_factor
-                measurements.append(measurement)
-            if not line[2] == "":
-                source_path = line[2]
-                file_name = source_path.split('/')[-1]
-                current_path = directory + 'IMG/' + file_name
-                image = cv2.imread(current_path)
-                images.append(image)
-                measurement = float(line[3]) - correction_factor
-                measurements.append(measurement)
+        # loop through frames and store images and measurements
+        for line in lines:
+            source_path = line[0]
+            file_name = source_path.split('/')[-1]
+            current_path = directory + data_path + 'IMG/' + file_name
+            image = mpimg.imread(current_path)
+            images.append(image)
+            measurement = float(line[3])
+            measurements.append(measurement)
+            if use_multiple_cameras:
+                if not line[1] == "":
+                    source_path = line[1]
+                    file_name = source_path.split('/')[-1]
+                    current_path = directory + data_path + 'IMG/' + file_name
+                    image = mpimg.imread(current_path)
+                    images.append(image)
+                    measurement = float(line[3]) + correction_factor
+                    measurements.append(measurement)
+                if not line[2] == "":
+                    source_path = line[2]
+                    file_name = source_path.split('/')[-1]
+                    current_path = directory + data_path + 'IMG/' + file_name
+                    image = mpimg.imread(current_path)
+                    images.append(image)
+                    measurement = float(line[3]) - correction_factor
+                    measurements.append(measurement)
 
     # data augmentation
     if data_augmentation:
@@ -79,11 +89,11 @@ def extend_with_flipped_images(images,measurements):
         # flipped data
         images.append(cv2.flip(images[i],1))
         measurements.append(measurements[i]*-1.0)
-        
+
     print("After data augmentation = " + str(len(images)))
 
-def draw_data(X,y):
-    n, bins, patches = plt.hist(y, 29, facecolor='g', alpha=0.75)
+def draw_data(y):
+    plt.hist(y, 29, facecolor='g', alpha=0.75)
     plt.title('Histogram of steering angles')
     plt.grid(True)
     plt.show()
